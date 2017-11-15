4d2982a5466a5938c9b7e80f48e6c88b47976c2a
Adrien <adrien@arccoza.com>
2017-11-15 12:29:45 +0900

    Update bench

--- bench.js
+++ bench.js
@@ -8,7 +8,7 @@ if (require && require.main === module) {
 
   var start = process.hrtime()
   for(var i = 0; i < 100000; i++)
-    var html = h('div#bob.a.b.c[type=awe][style=background:red; color:green]', {hola: 'value', class: ['c', 'foo', 'bar'], className: ['bar', 'baz'], style: {color: 'orange'}}, h('span', h('i', 'she\nsells\nsea', 'shells by the sea shore'), h('br'), h('i', {'eh': true})))
+    var html = h('div#bob.a.b.c[type=awe][style=background-color:red; color:green]', {hola: 'value', class: ['c', 'foo', 'bar'], className: ['bar', 'baz'], style: {backgroundColor: 'purple', color: 'orange'}}, h('span', h('i', 'she\nsells\nsea', 'shells by the sea shore'), h('br'), h('i', {'eh': true})))
   print(process.hrtime(start))
 
   print(html)
